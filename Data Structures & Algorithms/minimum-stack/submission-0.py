12d24c690e903a7ce7b3c375ba7f41f143317b71
AyatiD <147309139+AyatiD@users.noreply.github.com>
2026-09-22 18:28:20 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,25 @@
+#2 stacks, 1 to do all pop push top functions & secoond stack whose top always contains the minimum at that very level
+class MinStack:
+
+    def __init__(self):
+        self.stack=[]
+        self.min_stack=[]
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+        if not self.min_stack:
+            self.min_stack.append(val)
+        else:
+            self.min_stack.append(min(val, self.min_stack[-1]))
+        
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+        
